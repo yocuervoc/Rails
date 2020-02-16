bf2d12cf247ad6000654fd2826d477748cac5e62
yocuervoc <yocuervoc@unal.edu.co>
2020-02-16 03:14:05 -0500

    update, new

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,32 @@
 class ImagesController < ApplicationController
     def create
-        render plain: params[:image].inspect
+        #render plain: params[:image].inspect
         @imag= Image.new image_params
         @imag.save
+
+        redirect_to @imag
+    end
+
+    def show
+        @imag = Image.find params[:id]
     end
 
     def new
-        
+        @imag = Image.new
+    end
+
+    def index
+        @imag = Image.all
+    end
+
+    def edit
+        @imag = Image.find params[:id]
+    end
+
+    def update
+        @imag= Image.find params[:id]
+        @imag.update image_params
+        redirect_to @imag
     end
 
     private
